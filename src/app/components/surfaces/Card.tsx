6884813efcf33273650ed60dd2e50f4801e0f87d
Jennie Andersson <jennie.andersson@vinnovera.se>
2024-10-28 20:02:46 +0100

    feat: add CardRow and Pillar components; enhance Card and Textbox layouts; update H2 for uppercase support;

--- src/app/components/surfaces/Card.tsx
+++ src/app/components/surfaces/Card.tsx
@@ -1,3 +1,4 @@
+import Image, { StaticImageData } from 'next/image'
 import H2 from '../typography/H2'
 import P from '../typography/P'
 
@@ -5,34 +6,44 @@ type CardProps = {
   title: string
   paragraphs: string[]
   bgColor: string // TODO: Define and set specific color types
-  image?: string // Optional image for desktop layout
+  image?: StaticImageData | string
   style?: React.CSSProperties
+  oneColumn?: boolean
+  fullWidth?: boolean
+  bgRight?: boolean
+  bgLeft?: boolean
+  bgCenter?: boolean
 }
 
-const Card = ({ title, paragraphs, bgColor, image }: CardProps) => {
+const Card = ({
+  title,
+  paragraphs,
+  bgColor,
+  image,
+  oneColumn,
+  fullWidth,
+  bgRight,
+  bgLeft
+}: CardProps) => {
   return (
     <div
-      className={`p-6 ${bgColor} relative flex h-[561px] w-[331px] flex-col items-center rounded-[5px_0_0_0]`}
+      className={`p-6 ${bgColor} relative ${!fullWidth && 'w-4/5 max-w-[350px]'} flex-shrink-0`}
     >
       {/* Pseudo-element for background image */}
-      <div className="absolute inset-0 z-0 bg-card-pattern bg-200 opacity-5" />
+      <div
+        className={`${bgLeft && 'bg-left-top'} ${bgRight && 'bg-right-top'} absolute inset-0 z-0 bg-card-pattern bg-200 bg-no-repeat opacity-5`}
+      />
 
       {/* H2 with white text */}
       <H2 color="text-white">{title}</H2>
 
-      <div className="mt-2 flex flex-col items-center">
-        {' '}
-        
+      <div
+        className={`columns-1 gap-4 ${oneColumn ? 'columns-1' : 'lg:columns-3'}`}
+      >
         {/* Image column, only shown if an image is provided */}
         {image && (
-          <div className="mb-2">
-            {' '}
-            {/* Lägg till marginal under bilden */}
-            <img
-              src={image}
-              alt="Card Image"
-              className="h-[151px] w-[152px] rounded-lg opacity-100" // Specifik storlek för bilden
-            />
+          <div className="mb-2 text-center">
+            <Image className="m-auto" src={image} alt={''} />
           </div>
         )}
         {paragraphs.map((paragraph, idx) => (
--- src/app/components/typography/H2.tsx
+++ src/app/components/typography/H2.tsx
@@ -1,11 +1,14 @@
 type H2Props = {
   children: React.ReactNode
-  color?: string  // Färg som prop
+  color?: string // Färg som prop
+  upperCase?: boolean
 }
 
-const H2 = ({ children, color = 'text-black' }: H2Props) => {
+const H2 = ({ children, color = 'text-black', upperCase }: H2Props) => {
   return (
-    <h2 className={`font-josefin-sans ${color} text-2xl font-light leading-tight`}>
+    <h2
+      className={`font-josefin-sans ${color} mt-4 text-2xl font-light leading-tight ${upperCase && 'uppercase'}`}
+    >
       {children}
     </h2>
   )
