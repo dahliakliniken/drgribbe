e62b5e2922c93a6e5feded3254cf8175de5cf919
Jennie Andersson <jennie.andersson@vinnovera.se>
2024-10-30 21:37:11 +0100

    fix: adjust width on cards

--- src/app/components/surfaces/CardRow.tsx
+++ src/app/components/surfaces/CardRow.tsx
@@ -7,9 +7,8 @@ type CardRowProps = {
 const CardRow = ({ children }: CardRowProps) => {
   return (
     <div className="px-gapSpace flex space-x-4 overflow-x-auto lg:grid lg:grid-cols-4 lg:items-stretch">
-      {/* <div className="flex space-x-4 overflow-x-auto px-4 lg:grid lg:grid-cols-4 lg:px-0"> */}
       {Children.map(children, (child, index) => (
-        <div key={index} className="w-3/4 flex-none sm:w-1/2 lg:w-auto">
+        <div key={index} className="w-[90%] flex-none sm:w-1/2 lg:w-auto">
           {child}
         </div>
       ))}
