85e35c013e4bc34df83f6fedc77daa64e7a5f1d6
Jennie Andersson <jennie.andersson@vinnovera.se>
2024-11-25 18:38:27 +0100

    feat: add PriceList component and integrate pricing details in Priser page

--- src/app/components/typography/H2.tsx
+++ src/app/components/typography/H2.tsx
@@ -1,13 +1,16 @@
 type H2Props = {
   children: React.ReactNode
+  id?: string
   white?: boolean
   upperCase?: boolean
+  className?: string
 }
 
-export const H2 = ({ children, white, upperCase }: H2Props) => {
+export const H2 = ({ children, id, white, upperCase, className }: H2Props) => {
   return (
     <h2
-      className={`text-3xl font-light ${upperCase && 'uppercase'} ${white ? 'text-white' : 'text-black'} mb-4`}
+      id={id}
+      className={`text-3xl font-light ${upperCase && 'uppercase'} ${white ? 'text-white' : 'text-black'} mb-4 ${className}`}
     >
       {children}
     </h2>
