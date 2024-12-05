742dab871c356cc21df96de340767d4c811cc871
Jennie Andersson <jennie.andersson@vinnovera.se>
2024-12-05 18:16:05 +0100

    feat: adjust SpaceContainer usage in Brostoperationer component for improved layout

--- src/app/components/layout/Pillar.tsx
+++ src/app/components/layout/Pillar.tsx
@@ -1,3 +1,3 @@
 export const Pillar = ({ children }: { children: React.ReactNode }) => {
-  return <div className="mx-auto max-w-3xl px-4 py-8">{children}</div>
+  return <div className="mx-auto max-w-3xl py-8">{children}</div>
 }
