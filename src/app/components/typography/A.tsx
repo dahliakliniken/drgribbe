62c66b4b4df5b0356f9f38af118ec9e84936b514
Jennie Andersson <jennie.andersson@vinnovera.se>
2024-12-12 20:40:44 +0100

    feat: replace Button components with A components for booking links and add Boka page

--- src/app/components/typography/A.tsx
+++ src/app/components/typography/A.tsx
@@ -7,17 +7,33 @@ type AProps = {
   className?: string
   color?: string
   onClick?: (e: React.MouseEvent<HTMLAnchorElement>) => void
+  buttonStyle?: boolean
+  inverted?: boolean
+  small?: boolean
 }
 
 export const A = forwardRef<HTMLAnchorElement, AProps>(
-  ({ children, href, color = 'text-white', className, onClick }, ref) => {
+  (
+    {
+      children,
+      href,
+      color = 'text-white',
+      className,
+      onClick,
+      buttonStyle,
+      inverted,
+      small
+    },
+    ref
+  ) => {
+    const baseClass =
+      'font-josefin-sans text-base font-light leading-tight tracking-[0.06em]'
+    const buttonClass = `rounded-xl ${inverted ? 'border-black text-black hover:border-slate-500 hover:text-slate-500' : 'border-white text-white hover:border-beige hover:text-beige'} bg-transparent ${small ? 'border-2 p-2 text-small' : 'border-4 px-16 py-5 text-lg'}`
+    const linkClass = `hover:underline underline underline-offset-3 ${color}`
+    const combinedClass = `${baseClass} ${className} ${buttonStyle ? buttonClass : linkClass}`
+
     return (
-      <Link
-        href={href}
-        className={`underline-offset-3 font-josefin-sans text-base font-light leading-tight tracking-[0.06em] underline ${color} ${className}`}
-        ref={ref}
-        onClick={onClick}
-      >
+      <Link href={href} className={combinedClass} ref={ref} onClick={onClick}>
         {children}
       </Link>
     )
