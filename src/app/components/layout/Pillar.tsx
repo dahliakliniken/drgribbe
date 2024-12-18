2e8f5f7d5434dee5a1e898e09105071e2cfa81a5
Jennie Andersson <jennie.andersson@vinnovera.se>
2024-12-18 20:23:10 +0100

    feat: add FragorOchSvar component with FAQ functionality and integrate into navigation

--- src/app/components/layout/Pillar.tsx
+++ src/app/components/layout/Pillar.tsx
@@ -1,3 +1,13 @@
-export const Pillar = ({ children }: { children: React.ReactNode }) => {
-  return <div className="mx-auto max-w-3xl py-8">{children}</div>
+export const Pillar = ({
+  id,
+  children
+}: {
+  id?: string
+  children: React.ReactNode
+}) => {
+  return (
+    <div className="mx-auto max-w-3xl py-8" id={id}>
+      {children}
+    </div>
+  )
 }
