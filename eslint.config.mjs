ef05200a61dca2b4d620deb503a8bb727c8daf81
Jennie Andersson <jennie.andersson@vinnovera.se>
2025-02-21 10:11:33 +0100

    feat: update ESLint configuration to include Next.js support and add new dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,22 +3,33 @@ import pluginJs from '@eslint/js'
 import tseslint from 'typescript-eslint'
 import pluginReact from 'eslint-plugin-react'
 import eslintConfigPrettier from 'eslint-config-prettier'
+import nextPlugin from '@next/eslint-plugin-next'
+import { FlatCompat } from '@eslint/eslintrc'
 
-export default [
+const compat = new FlatCompat()
+
+const eslintConfig = [
   { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  ...compat.config({ extends: ['next'] }),
   {
-    ignores: ['node_modules', 'dist', 'public', '.next']
-  },
-  {
+    plugins: {
+      '@next/next': nextPlugin
+    },
     rules: {
+      ...nextPlugin.configs.recommended.rules,
       'react/react-in-jsx-scope': 'off',
       'react/jsx-uses-react': 'off',
       'react/jsx-no-literals': 'error'
     }
   },
+  {
+    ignores: ['node_modules', 'dist', 'public', '.next']
+  },
   eslintConfigPrettier
 ]
+
+export default eslintConfig
