a171ec0a6735349343fcaae03caa2d12aae3f8ea
Jennie Andersson <jennie.andersson@vinnovera.se>
2025-03-25 19:38:40 +0100

    fix: update site URL in sitemap configuration

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://www.dahliakliniken.se',
+  siteUrl: process.env.SITE_URL || 'https://dahliakliniken.se',
   generateRobotsTxt: true
 }
