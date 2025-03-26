b2cd1bdbab7a0b5c7e54b2007e4936bd4dd012fb
Harald Mellbom <harald@mellbom.se>
2025-03-26 16:24:56 +0100

    policy page is done

---
+++ src/app/personuppgiftspolicy/page.tsx
@@ -0,0 +1,19 @@
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Vår personuppgiftspolicy - Dahliakliniken',
+  description:
+    'Vår personuppgiftspolicy - Dahliakliniken. Här kan du läsa vår personuppgiftspolicy',
+  openGraph: {
+    title: 'Vår personuppgiftspolicy - Dahliakliniken',
+    description:
+      'Vår personuppgiftspolicy - Dahliakliniken. Här kan du läsa vår personuppgiftspolicy'
+  },
+  twitter: {
+    title: 'Vår personuppgiftspolicy - Dahliakliniken',
+    description:
+      'Vår personuppgiftspolicy - Dahliakliniken. Här kan du läsa vår personuppgiftspolicy'
+  }
+}
+
+export { default } from './personuppgiftspolicy'
