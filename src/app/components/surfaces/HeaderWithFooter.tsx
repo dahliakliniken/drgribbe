752db1ad1a11de2a6857bfe7fefefd592328b3ff
Jennie Andersson <jennie.andersson@vinnovera.se>
2025-06-02 21:14:44 +0200

    fix: reorder imports for consistency in HeaderWithFooter component

--- src/app/components/surfaces/HeaderWithFooter.tsx
+++ src/app/components/surfaces/HeaderWithFooter.tsx
@@ -1,7 +1,7 @@
 'use client'
 
-import { useTranslations } from 'next-intl'
 import { usePathname } from 'next/navigation'
+import { useTranslations } from 'next-intl'
 import { useEffect, useState } from 'react'
 
 import { Button } from '../inputs/Button'
