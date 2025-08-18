59fbfb91ed7346e1fa9518d15eec91a531b4ef94
Jennie Andersson <jennie.andersson@vinnovera.se>
2025-08-18 20:12:44 +0200

    chore: update packages

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,7 +6,7 @@ export function middleware(request: NextRequest) {
     default-src 'self';
     script-src 'self' 'unsafe-eval' 'unsafe-inline' 'nonce-${nonce}' 'strict-dynamic' https://www.googletagmanager.com https://www.google-analytics.com https://connect.facebook.net https://www.facebook.com https://cookie-script.com https://*.cookie-script.com https://pagead2.googlesyndication.com;
     style-src 'self' 'unsafe-inline' https://cookie-script.com https://*.cookie-script.com;
-    font-src 'self' https://js.stripe.com;
+    font-src 'self';
     img-src 'self' blob: data: https://www.google-analytics.com https://www.googletagmanager.com https://www.facebook.com https://www.facebook.com/privacy_sandbox/pixel/register https://cookie-script.com https://pagead2.googlesyndication.com;
     connect-src 'self' https://www.google-analytics.com https://analytics.google.com https://*.google-analytics.com https://www.facebook.com https://graph.facebook.com https://cookie-script.com https://pagead2.googlesyndication.com;
     frame-src 'self' https://app.meridiq.com https://www.google.com https://*.google.com https://www.googletagmanager.com https://www.facebook.com https://cookie-script.com https://vercel.live;
