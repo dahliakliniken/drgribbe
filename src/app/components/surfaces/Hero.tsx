7d0e230de628e02327fcc0fc600bd50a5ef36a39
Jennie Andersson <jennie.andersson@vinnovera.se>
2026-02-18 20:40:20 +0100

    feat: add hero video

--- src/app/components/surfaces/Hero.tsx
+++ src/app/components/surfaces/Hero.tsx
@@ -2,16 +2,17 @@
 
 import classNames from 'classnames'
 import { ChevronDown } from 'lucide-react'
-import Image from 'next/image'
 import { useTranslations } from 'next-intl'
 
 import { useDelayedAnimation } from '@/hooks/useDelayedAnimation'
 import HeroImage from '@/public/images/hero-image.jpg'
 
 import { A } from '../typography/A'
+import { Video } from './Video'
+
+const HERO_VIDEO_SRC = '/videos/hero.mp4'
 
 export const Hero = () => {
-  const tPage = useTranslations('home-page.page')
   const tCommon = useTranslations('common')
   const showAnimation = useDelayedAnimation(3000) // 3 seconds delay
 
@@ -23,26 +24,25 @@ export const Hero = () => {
   }
 
   return (
-    <div
-      className={classNames('transition-all duration-500', {
-        'animate-page-bounce': showAnimation
-      })}
-    >
       <section className="relative flex h-[calc(100dvh-80px)] flex-col items-center justify-center">
         <div className="absolute inset-0">
-          <Image
-            src={HeroImage}
-            className="h-full w-full object-cover object-center"
-            priority
-            placeholder="blur"
-            alt={tPage('altText.heroImage')}
+          <Video
+            src={HERO_VIDEO_SRC}
+            className="h-full w-full"
+            autoPlay
+            muted
+            loop
+            preload="metadata"
+            poster={HeroImage.src}
+            fallbackImageSrc={HeroImage.src}
+            fallbackImageAlt=""
           />
         </div>
 
         <div className="herogradient absolute inset-0"></div>
         <div
           className={classNames(
-            'absolute bottom-4 left-1/2 -translate-x-1/2 cursor-pointer transition-opacity duration-500',
+            'absolute bottom-4 left-1/2 z-10 -translate-x-1/2 cursor-pointer transition-opacity duration-500',
             {
               'opacity-100': showAnimation,
               'opacity-0': !showAnimation
@@ -52,12 +52,11 @@ export const Hero = () => {
         >
           <ChevronDown size={48} className="animate-bounce text-white" />
         </div>
-        <div className="absolute bottom-24">
+        <div className="absolute bottom-24 z-20">
           <A href="/boka" buttonStyle>
             {tCommon('bookConsultation')}
           </A>
         </div>
       </section>
-    </div>
   )
 }
