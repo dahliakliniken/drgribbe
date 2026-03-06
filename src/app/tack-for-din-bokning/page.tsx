b46e711be2db2c01a8bc6c7b1899d5ec5222314c
Jennie Andersson <jennie.andersson@vinnovera.se>
2026-03-06 16:36:13 +0100

    fix: update import path for Tack component in tack page

--- src/app/tack-for-din-bokning/page.tsx
+++ src/app/tack-for-din-bokning/page.tsx
@@ -1,6 +1,6 @@
 import { Metadata } from 'next'
 
-import { Tack } from '@/app/tack/Tack'
+import { Tack } from '@/app/tack-for-din-bokning/Tack'
 
 // TODO: Change the metadata to be more specific to the tack page
 
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
   robots: {
     index: false,
     follow: false
-  },
+  }
 }
 
 function TackPage() {
